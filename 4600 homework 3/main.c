07819a26ee00c147cad8a11e41a62235c1fca41b
Jason Eastman <jasont41@gmail.com>
2020-10-01 21:36:51 -0700

    Initial Commit

---
+++ 4600 homework 3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  4600 homework 3
+//
+//  Created by jason eastman on 10/1/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
